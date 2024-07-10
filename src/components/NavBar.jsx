2a78b869acec1ef56a3e8d2a7793b5cdff721adf
Govind Rajewar <abhirajewar87@gmail.com>
2024-07-10 14:38:19 +0530

    Added NavBar Component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import "../style/NavBar.css";
+import logo from "../assets/NavBar/logo.jpeg";
+import { IoMoon } from "react-icons/io5";
+
+function NavBar() {
+  return (
+    <div className="NavBar">
+      <div className="title">
+        <div>
+          <img src={logo} alt="" className="logo" />
+        </div>
+        <div className="title-name">Nexcent</div>
+      </div>
+      <div className="navigation-bar">
+        <a href="#">Home</a>
+        <a href="#">Services</a>
+        <a href="#">Features</a>
+        <a href="#">Product</a>
+        <a href="#">Testimonial</a>
+        <a href="#">FAQ</a>
+      </div>
+      <div className="buttons">
+      <IoMoon className="moon"/>
+        <button>Sign Up</button>
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
